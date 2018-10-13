94a333d56efbf8c4a8d217d5fef36aaca0743430
Michael Dryden <pudds55@gmail.com>
2018-10-13 12:04:27 -0500

    cleanup

---
+++ src/v1/Tests/Models/DeserializationTestsBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using mdryden.cflapi.v1.Models;
+using mdryden.cflapi.v1.Models.Games;
+using mdryden.cflapi.v1.Models.Players;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+using mdryden.cflapi.v1.Client;
+
+namespace mdryden.cflapi.v1.Tests.Models
+{
+	public class DeserializationTestsBase
+	{
+
+		public DeserializationTestsBase()
+		{
+			var apiKey = ConfigurationManager.AppSettings["apiKey"];
+			Endpoint.Setup(apiKey);
+		}
+	}
+}
